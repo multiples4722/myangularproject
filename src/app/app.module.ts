b359457fad42c7e8327357deed208551495d0b8c
multiples4722 <blankson.daniel@philander.edu>
2020-08-18 22:31:25 -0400

    uploads

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,66 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ResearchComponent } from './research/research.component';
+import { EducationComponent } from './education/education.component';
+import { PartnersComponent } from './partners/partners.component';
+import { PeopleComponent } from './people/people.component';
+import {Routes, RouterModule} from "@angular/router";
+import { HeaderComponent } from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ContentComponent } from './content/content.component';
+
+
+const appRoutes:Routes=[
+  {
+    path: 'home',
+    component: HomeComponent
+},
+{
+  path: 'research',
+  component: ResearchComponent
+},
+{
+  path: 'education',
+  component: EducationComponent
+},
+{
+  path: 'partners',
+  component: PartnersComponent
+},
+{
+  path: 'people',
+  component: PeopleComponent
+},
+{
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+},
+{
+  path: '**',
+  redirectTo: '/home',
+  pathMatch: 'full'
+}
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResearchComponent,
+    EducationComponent,
+    PartnersComponent,
+    PeopleComponent,
+    HeaderComponent,
+    CarouselComponent,
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
